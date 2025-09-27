39574f64edc60316a1e935ad38ed52126900849b
Zsolt Harmath <harmathzs@gmail.com>
2025-09-27 11:15:12 +0200

    Add social login buttons

--- react-app/src/pages/FileUploadPage.jsx
+++ react-app/src/pages/FileUploadPage.jsx
@@ -2,8 +2,13 @@ import React from "react";
 export default class FileUploadPage extends React.Component {
     render() {
         return (
-            <div>
-                FileUploadPage
+            <div id="content-file" className="tab-content active">
+                <h2>File Upload</h2>
+                <form>
+                    <label htmlFor="fileInput">Choose file to upload:</label>
+                    <input type="file" id="fileInput" name="fileInput" multiple />
+                    <button type="submit">Upload</button>
+                </form>
             </div>
         )
     }
